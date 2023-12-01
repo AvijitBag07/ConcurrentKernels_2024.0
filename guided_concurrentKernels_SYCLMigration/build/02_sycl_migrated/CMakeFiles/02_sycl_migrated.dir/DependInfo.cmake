81bddbf866e32d3ceffc82fe80247f25f62dce26
AvijitBag07 <144990856+AvijitBag07@users.noreply.github.com>
2023-12-01 11:30:42 +0530

    Add files via upload

---
+++ guided_concurrentKernels_SYCLMigration/build/02_sycl_migrated/CMakeFiles/02_sycl_migrated.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/u197045/Avijit/oneAPI-samples/DirectProgramming/C++SYCL/guided_concurrentKernels_SYCLMigration/02_sycl_migrated/Samples/0_Introduction/concurrentKernels/concurrentKernels.dp.cpp" "02_sycl_migrated/CMakeFiles/02_sycl_migrated.dir/Samples/0_Introduction/concurrentKernels/concurrentKernels.dp.cpp.o" "gcc" "02_sycl_migrated/CMakeFiles/02_sycl_migrated.dir/Samples/0_Introduction/concurrentKernels/concurrentKernels.dp.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ guided_concurrentKernels_SYCLMigration/build/02_sycl_migrated/CMakeFiles/02_sycl_migrated.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/02_sycl_migrated"
+  "../bin/02_sycl_migrated.pdb"
+  "CMakeFiles/02_sycl_migrated.dir/Samples/0_Introduction/concurrentKernels/concurrentKernels.dp.cpp.o"
+  "CMakeFiles/02_sycl_migrated.dir/Samples/0_Introduction/concurrentKernels/concurrentKernels.dp.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_sycl_migrated.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
